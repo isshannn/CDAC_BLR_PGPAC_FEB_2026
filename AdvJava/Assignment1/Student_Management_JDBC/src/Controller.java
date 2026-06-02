ed75010a67f4acef7e996657a2f88a1b2f99d65c
DAC15 <DAC15@ACTS.CDACECBLR.COM>
2026-06-02 16:15:43 +0530

    Created Folder for Advanced Java Module

---
+++ AdvJava/Assignment1/Student_Management_JDBC/src/Controller.java
@@ -0,0 +1,16 @@
+
+public class Controller {
+	public static void main (String[] args) {
+		int ch = 0;
+		while(ch == 0) {
+			System.out.println("\n\n\n===== STUDENT MANAGEMENT SYSTEM =====");
+			System.out.println("1. Insert Student Record");			
+			System.out.println("2. Update Student Record");			
+			System.out.println("3. Delete Student Record");			
+			System.out.println("4. Display All Student Records");			
+			System.out.println("5. Search Student by ID");			
+			System.out.println("6. Exit");
+			System.out.println("Enter your Choice");
+		}
+	}
+}
